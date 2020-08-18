4e66af81d3ed66b26d92775634bf1e4ab4365633
Paulo Ravi Cáceres <paulocaceres17@gmail.com>
2020-08-18 08:12:33 -0500

    docs add

---
+++ docs/precache-manifest.9d976a3861f5a8c997f9f12cc5e3c28d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e00203c67fe4dae4c95634e35b2e28c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "459c6bd23d64b4004430",
+    "url": "/static/css/main.7294c5a8.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "459c6bd23d64b4004430",
+    "url": "/static/js/main.984ae062.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
